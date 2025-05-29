c59b731d26c4532645b4a21b9cf6cc378916daac
Phạm Tuấn Dũng <dungpt.pdm@gmail.com>
2025-05-29 14:34:37 +0700

    upload bot

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Xin chào, tôi là Chủ tịch Phạm Tuấn Dũng. Bạn cần gì?")
 
-app = ApplicationBuilder().token("7418220278:AAEm6L3b3ujbCVzE8i_qF-2sUddFFTdryWU").build()
+app = ApplicationBuilder().token("7868411673:AAFhQ2e8Mfm_aoVpB-YdZJC0O6RlNLAZJKM").build()
 app.add_handler(CommandHandler("start", start))
 
 print("Bot đang chạy...")
+await application.bot.delete_webhook(drop_pending_updates=True)
 app.run_polling()
