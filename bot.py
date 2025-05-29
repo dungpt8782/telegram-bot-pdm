c1d6145d3f4c15f49001c63fd9d7770313c2c9cb
Phạm Tuấn Dũng <dungpt.pdm@gmail.com>
2025-05-29 14:42:34 +0700

    fix await outside function

--- bot.py
+++ bot.py
@@ -2,11 +2,16 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Xin chào, tôi là Chủ tịch Phạm Tuấn Dũng. Bạn cần gì?")
+    await update.message.reply_text("Xin chào, tôi là Chủ tịch PDM Phạm Tuấn Dũng. Bạn cần gì?")
 
-app = ApplicationBuilder().token("7868411673:AAFhQ2e8Mfm_aoVpB-YdZJC0O6RlNLAZJKM").build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    application = ApplicationBuilder().token("7868411673:AAFhQ2e8Mfm_aoVpB-YdZJC0O6RlNLAZJKM").build()
 
-print("Bot đang chạy...")
-await application.bot.delete_webhook(drop_pending_updates=True)
-app.run_polling()
+    await application.bot.delete_webhook(drop_pending_updates=True)
+    application.add_handler(CommandHandler("start", start))
+
+    print("Bot đang chạy...")
+    await application.run_polling()
+
+import asyncio
+asyncio.run(main())
